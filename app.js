2f4162eef9eca38dbc8a92e712bce655975a537c
ibraheem ibraheem <iibraheem2001@gmail.com>
2021-09-14 14:38:10 -0700

    created a new repo for the about me assignment, will contnue to improve this over time

---
+++ app.js
@@ -0,0 +1,49 @@
+let ownHouse = prompt('Do you think I own a house?');
+if (ownHouse == 'yes'){
+    
+    alert('wrong, I dont own a house!');
+}else{
+    alert('you are correct, I dont own a house.')
+}
+
+
+let breakFast = prompt('Did I have breakfast today?');
+if (breakFast == 'yes'){
+
+    alert('You are right, I did have breakfast.')
+}else{
+    alert('you are wrong');
+
+}
+
+
+let ownCar = prompt('Do I own a car?');
+if (ownCar == 'yes'){
+
+    alert('you are right, I do own a car!')
+}else{
+
+    alert('Wrong, I do own a car');
+}
+
+let sellHouses = prompt('Do I sell houses?');
+if (sellHouses == 'yes'){
+    
+    alert('you are right, I do sell houses!')
+}else{
+    
+    alert('Wrong, its my job to sell houses');
+}
+
+let howOld = prompt('am I 20 years old?');
+if (howOld == 'yes'){
+    
+    alert('you are right, I turned 20 this month!')
+}else{
+    
+    alert('Wrong, I just turned 20 this month');
+}
+console.log(
+    
+    
+    )
